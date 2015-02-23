91cf3a9dc8860b30bae541a26d1832bc79eef96c
Dan Wysocki <dwysocki@oswego.edu>
2015-02-22 23:33:27 -0500

    Created project metadata

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject hidden-markov-music "0.1.0-SNAPSHOT"
+  :description "Generate original musical scores by means of a hidden Markov
+                model."
+  :url "https://github.com/dwysocki/hidden-markov-music"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [overtone            "0.9.1"]]
+  :main hidden-markov-music.core)
