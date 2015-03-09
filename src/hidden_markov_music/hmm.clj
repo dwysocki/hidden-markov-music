6a1e524097f3228ec2135cb7d5b51f921fdfd294
Dan Wysocki <dwysocki@oswego.edu>
2015-03-08 23:15:17 -0400

    Changed some arguments and removed unused code, but did not add much

--- src/hidden_markov_music/hmm.clj
+++ src/hidden_markov_music/hmm.clj
@@ -1,41 +1,41 @@
 (ns hidden-markov-music.hmm
   (:require [hidden-markov-music.stats :as stats]))
 
-(defrecord HMM [state-transition-probs
+(defrecord HMM [possible-states
+                possible-observations
+                state-transition-probs
                 observation-probs
                 initial-state-dist])
-
-(defn- random-state-transition-probs
-  "Returns a randomly generated transition probability map."
-  [possible-states]
-  (let [n-states (count possible-states)]
-    (into {}
-          (for [state possible-states]
-            (let [row-probs (stats/random-stochastic-vector n-states)]
-              [state
-               (into {}
-                     (map (fn [s p] [s p])
-                          possible-states
-                          row-probs))])))))
 
 (defn random-hmm
   "Returns a model with random probabilities."
   [possible-states possible-observations]
   (HMM.
-   (stats/random-row-stochastic-map possible-states
-                                    possible-states)
-   (stats/random-row-stochastic-map possible-states
-                                    possible-observations)
-   (stats/random-stochastic-map possible-states)))
+    possible-states
+    possible-observations
+    (stats/random-row-stochastic-map possible-states
+                                     possible-states)
+    (stats/random-row-stochastic-map possible-states
+                                     possible-observations)
+    (stats/random-stochastic-map possible-states)))
+
+(defn- alpha
+  "Returns α_{T-1}(i)"
+  [model observations state]
+  ; ...
+  )
 
 (defn forward-probabilities
   "Returns P(O|λ), the likelihood of the observed sequence given the model."
-  [observations model])
+  [model observations]
+  (reduce + (map #(alpha model observations %)
+                 (:possible-states model))))
+
 
 (defn backward-probabilities
   "Returns X, the optimal hidden state sequence given the observed sequence and
   model."
-  [observations model])
+  [model observations])
 
 (defn posterior-marginals
   "Returns the distribution P(X|O), the posterior marginals of all hidden state
@@ -44,4 +44,4 @@
 
 (defn train
   "Trains a model on an observed sequence, using the Baum-Welch algorithm."
-  ([observations model]))
+  ([model observations]))
